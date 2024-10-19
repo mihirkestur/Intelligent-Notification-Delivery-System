de8cea5c7aa3948a01f2978c79a4215d942b06a1
mihirkestur <mihirkestur@gmail.com>
2024-10-19 17:34:23 -0400

    Multi App Stage

--- rl_agent/NotifRLEnv.py
+++ rl_agent/NotifRLEnv.py
@@ -7,18 +7,28 @@ class NotificationTimingEnv(gym.Env):
         super(NotificationTimingEnv, self).__init__()
         
         # State: Time of the day when the agent can send a notification (continuous between 0 and 1)
-        self.observation_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
+        # self.observation_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Dict({
+            'time_of_day': spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32),
+            'app_type': spaces.Discrete(2)  # 0: app X, 1: app Y
+        })
         
         # Action: Time to send a notification (continuous between 0 and 1)
         self.action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
         
         # Initialize Send Notification Time 
         self.send_notification_time = np.random.uniform(0, 1)
+        self.app_type = np.random.choice([0, 1])
 
     def reset(self, seed=None):
-        # Reset the environment state at midnight (or another point in time)
         self.send_notification_time = np.random.uniform(0, 1)
-        return np.array([self.send_notification_time], dtype=np.float32), {}
+        self.app_type = np.random.choice([0, 1])  # Randomly set the app (app X or app Y)
+        
+        # Return the initial observation
+        return {
+            'time_of_day': np.array([self.send_notification_time], dtype=np.float32),
+            'app_type': self.app_type
+        }, {}
     
     def step(self, action):
         # The action represents the time to send the notification (between 0 and 1)
@@ -39,7 +49,12 @@ class NotificationTimingEnv(gym.Env):
         done = 1
 
         # Return the next state (Send Notification Time), reward, and done flag
-        return np.array([self.send_notification_time], dtype=np.float32), reward, done, False, {}
+        # return np.array([self.send_notification_time], dtype=np.float32), reward, done, False, {}
+        return {
+            'time_of_day': np.array([self.send_notification_time], dtype=np.float32),
+            'app_type': self.app_type
+        }, reward, done, False, {}
+        
     
     def render(self, mode="human"):
         # Print Send Notification Time and action
@@ -47,4 +62,7 @@ class NotificationTimingEnv(gym.Env):
 
     # Example function to simulate the open time based on notification arrival time
     def simulate_open_time(self, notif_arrive_time):
-        return 0.9
+        if self.app_type == 0:
+            return 0.5
+        else:
+            return 0.9
--- rl_agent/main.py
+++ rl_agent/main.py
@@ -4,20 +4,24 @@ import numpy as np
 # Create the environment
 env = NotificationTimingEnv()
 
-# Train an RL agent
-model = DDPG("MlpPolicy", env, verbose=1)
-# model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=1000)
-# Save the model
-model.save("./models/notification_model_test")
+# # Train an RL agent
+# model = DDPG("MultiInputPolicy", env, verbose=1)
+# # model = PPO("MlpPolicy", env, verbose=1)
+# model.learn(total_timesteps=1000)
+# # Save the model
+# model.save("./models/notification_model_xy_apps")
 
 
-# # Load the saved model
-# # model = PPO.load("./models/notification_model")
-# model = DDPG.load("./models/notification_model_test")
-# # Test the trained agent
-# action, _states = model.predict(np.array([0.9]), deterministic=True)
-# print(action, _states)
+# Load the saved model
+# model = PPO.load("./models/notification_model")
+model = DDPG.load("./models/notification_model_xy_apps")
+# Test the trained agent
+observation = {
+    "time_of_day": np.array([0.1], dtype=np.float32),
+    "app_type": np.array([1], dtype=np.float32),
+}
+action, _states = model.predict(observation, deterministic=True)
+print(action, _states)
 # # obs, _ = env.reset()
 # # env.send_notification_time = np.array([99])
 # # obs, reward, done, truncated, info = env.step(action)
